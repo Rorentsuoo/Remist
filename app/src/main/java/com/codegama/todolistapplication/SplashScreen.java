1d953edebf5f5d82153c81f8803f2dec4972a5e5
L1PC28 Student <rorentsuopersonal@gmail.com>
2023-06-05 11:36:43 +0800

    Remist

---
+++ app/src/main/java/com/codegama/todolistapplication/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.codegama.todolistapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.codegama.todolistapplication.activity.MainActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, Login.class));
+                finish();
+            }
+        },3000);
+    }
+}
